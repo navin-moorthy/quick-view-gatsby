5afa349465f85eb4ae285e0ca73ac7f333862a5c
Navin <navin007.a@gmail.com>
2020-06-17 15:55:51 +0530

    feat: :sparkles: use the viewport center component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { Box, Text, useTheme } from "@chakra-ui/core"
+
 import SEO from "../components/seo"
+import { ViewportCenter } from "../components/viewport-center"
 
 const IndexPage = () => {
   const theme = useTheme()
   console.log("%c theme", "color: #ff0000", theme)
 
   return (
-    <Box p={8}>
+    <ViewportCenter>
       <SEO title="Home" />
       <Text fontSize="xl">Hello World</Text>
-    </Box>
+    </ViewportCenter>
   )
 }
 
