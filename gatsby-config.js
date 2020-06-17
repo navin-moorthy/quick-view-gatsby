ddccb27795e211f277079ad9ad67e9482e40aa23
Navin <navin007.a@gmail.com>
2020-06-17 22:23:44 +0530

    fix: :bug: remove colorMode from Chakra UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-chakra-ui",
+    {
+      resolve: "gatsby-plugin-chakra-ui",
+      options: {
+        isUsingColorMode: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
