9bd8828df10dc6c12f5995a56029b3218900d308
Navin <navin007.a@gmail.com>
2020-06-17 15:15:25 +0530

    feat: :sparkles: add the chakra base components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react"
-
+import { Box, Text, useTheme } from "@chakra-ui/core"
 import SEO from "../components/seo"
 
-const IndexPage = () => <SEO title="Home" />
+const IndexPage = () => {
+  const theme = useTheme()
+  console.log("%c theme", "color: #ff0000", theme)
+
+  return (
+    <Box p={8}>
+      <SEO title="Home" />
+      <Text fontSize="xl">Hello World</Text>
+    </Box>
+  )
+}
 
 export default IndexPage
