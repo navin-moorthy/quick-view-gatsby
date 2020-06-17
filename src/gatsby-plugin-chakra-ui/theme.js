8f94071af045b4fc0a1ef2df0b1ec42682f191df
Navin <navin007.a@gmail.com>
2020-06-17 16:24:29 +0530

    feat: :sparkles: add brand blue swatch

--- src/gatsby-plugin-chakra-ui/theme.js
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -6,7 +6,16 @@ const theme = {
     ...chakraTheme.colors,
     blue: {
       ...chakraTheme.colors.blue,
+      50: "#dcf7ff",
+      100: "#b4e3fb",
+      200: "#8acff2",
+      300: "#5ebbeb",
+      400: "#34a8e5",
       500: "#1B91CF",
+      600: "#0d6f9f",
+      700: "#024f73",
+      800: "#003048",
+      900: "#00121e",
     },
   },
   sizes: {
